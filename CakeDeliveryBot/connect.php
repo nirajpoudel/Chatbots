2d821e6daff3aef0dbef4fbb7013c035ec9c6342
Niraj <59787504+Niraj-Poudel@users.noreply.github.com>
2020-07-13 14:18:52 +0545

    Add files via upload

---
+++ CakeDeliveryBot/connect.php
@@ -0,0 +1,18 @@
+<?php
+$email = filter_input(INPUT_POST, 'email');
+$phone = filter_input(INPUT_POST, 'phone');
+if (!empty($email)){
+if (!empty($phone)){
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "Bot";
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+if (mysqli_connect_error()){
+die('Connect Error ('. mysqli_connect_errno() .') '. mysqli_connect_error());
+}
+else{
+$sql = "INSERT INTO UserInfo (Email, Phone) values ('$email','$phone')";
+}
+?>
